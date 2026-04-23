5abf39ea2d73e0341dcc6968b057b2622880b0d4
Pablo Henrique <pablo_hen@hotmail.com>
2026-04-23 08:29:14 -0300

    chore: update typecheck script to prevent emitting files and add strictNullChecks to TypeScript configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@vtex/shoreline/css'
-import { routeTree } from './routeTree.gen.ts'
+import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
 
 declare module '@tanstack/react-router' {
 	interface Register {
+		// This infers the type of our router and registers it across your entire project
 		router: typeof router
 	}
 }
